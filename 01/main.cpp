2aaa8c748f2b0bc875fb1a114db57329af2eb206
Jakub Krusicki <kkrusicki@gmail.com>
2024-03-04 23:45:18 +0100

    finished, but still some errors
    I don't understand the errors in the complex part

--- 01/main.cpp
+++ 01/main.cpp
@@ -36,7 +36,7 @@ public:
                 grades_.emplace_back(grade);
         }
         else
-            cout << "invalid"
+            cout << "invalid";
 
     }
     vector<float> grades(){
@@ -50,8 +50,49 @@ private:
     vector<float> grades_;
 };
 
+class Complex {
+    float real_;
+    float imaginary_;
+
+public:
+    Complex(float real, float im){
+        real_ = real;
+        imaginary_ = im;
+    }
+    Complex(float real){
+        real_ = real;
+        imaginary_ = 0.0;
+    }
+    void set_real(float real){
+        real_ = real;
+    }
+    float real(){
+        return real_;
+    }
+    void set_im(float imaginary){
+        imaginary_ = imaginary;
+    }
+    float imaginary(){
+        return imaginary_;
+    }
+    void add(Complex b){
+        real_ += b.real_;
+        imaginary_ += b.imaginary_;
+    }
+    void print(){
+        cout << real_ << imaginary_ << endl;
+    }
+};
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    Complex a(1.0, -2.0); // creates 1-2i
+    Complex b(3.14); // creates 3.14
+
+    b.set_im(-5);
+
+    Complex c = a.add(b); // I don't really understand the error here
+
+    c.print(); // prints 4.14-7i
     return 0;
 }
