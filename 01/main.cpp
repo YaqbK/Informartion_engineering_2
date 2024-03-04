3df7e0cf0b6e0edba201fa03ce22318f46dccb01
Jakub Krusicki <kkrusicki@gmail.com>
2024-03-04 23:25:02 +0100

    first part of homework
    extended the student class

--- 01/main.cpp
+++ 01/main.cpp
@@ -1,6 +1,54 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
+
+class Student {
+public:
+    void set_index(int index) {
+        index_ = index;
+    }
+    int index() {
+        return index_;
+    }
+    void set_name(string name){
+        name_ = name;
+    }
+    string name(){
+        return name_;
+    }
+    void set_surname(string surname){
+        surname_ = surname;
+    }
+    string surname(){
+        return surname_;
+    }
+    void set_album_num(int album_num) {
+        album_num_ = album_num;
+    }
+    int album_num() {
+        return album_num_;
+    }
+    void add_grade(float grade){
+        if (grade >= 2.0){
+            if (grade <= 5.0)
+                grades_.emplace_back(grade);
+        }
+        else
+            cout << "invalid"
+
+    }
+    vector<float> grades(){
+        return grades_;
+    }
+private:
+    int index_;
+    string name_;
+    string surname_;
+    int album_num_;
+    vector<float> grades_;
+};
 
 int main()
 {
