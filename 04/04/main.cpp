c652781809878f8c58678766097170fe2cf2a348
Jakub Krusicki <kkrusicki@gmail.com>
2024-04-09 00:59:08 +0200

    update from classes
    I copied what i did during labs from justpasteit

--- 04/04/main.cpp
+++ 04/04/main.cpp
@@ -1,44 +1,40 @@
 #include <iostream>
-
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 
-
-class CustomRectangleShape : public sf::RectangleShape{
+    class CustomRectangleShape : public sf::RectangleShape{
 
 public:
-    CustomRectangleShape(const sf::Vector2f &size, const sf::Vector2f position){
 
+    CustomRectangleShape(const sf::Vector2f &size, const sf::Vector2f position){
         setPosition(position);
         setSize(size);
         setFillColor(sf::Color(95, 55, 230));
-
     }
-
     void setSpeed(const int &Vspeed, const int &Hspeed, const int &Rspeed){
         Xspeed_ = Vspeed;
         Yspeed_ = Hspeed;
         Rspeed_ = Rspeed;
     }
-
     void animate(float &dt, int y, int x){
         move(Yspeed_*dt, Xspeed_*dt);
         rotate(Rspeed_*dt);
-
-        sf::FloatRect rec_bounds = getGlobalBounds();
-
+         sf::FloatRect rec_bounds = getGlobalBounds();
         if(rec_bounds.top <= 0){
             Xspeed_ = abs(Xspeed_);
             setFillColor(sf::Color(rand() % 256, rand() % 256, rand() % 256));
         }
+
         if(rec_bounds.top + rec_bounds.height >= y){
             Xspeed_ = abs(Xspeed_) * -1;
             setFillColor(sf::Color(rand() % 256, rand() % 256, rand() % 256));
         }
+
         if(rec_bounds.left <= 0 ){
             Yspeed_ = abs(Yspeed_);
             setFillColor(sf::Color(rand() % 256, rand() % 256, rand() % 256));
         }
+
         if(rec_bounds.left + rec_bounds.width >= x){
             Yspeed_ = abs(Yspeed_) * -1;
             setFillColor(sf::Color(rand() % 256, rand() % 256, rand() % 256));
@@ -81,29 +77,30 @@ public:
         if (object1.left + object1.width >= object2.left && object1.left < object2.left){
             Yspeed_ = abs(Yspeed_)*-1;
             std::cout << "right" << std::endl;
-        }
-    }
 
+        }
+
+    }
 private:
+
     int Xspeed_ = 0;
     int Yspeed_ = 0;
     int Rspeed_ = 0;
-    // int lb_ = 0, rb_ = 0, tb_ = 0, bb_ = 0;
-
 };
+
 int main() {
     // create the window
     sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
-
     // create some shapes
     sf::CircleShape circle(50.0);
     circle.setPosition(100.0, 300.0);
     circle.setFillColor(sf::Color(100, 250, 50));
 
     sf::RectangleShape rectangle(sf::Vector2f(120.0, 60.0));
-    rectangle.setPosition(500.0, 400.0);
-    rectangle.setFillColor(sf::Color(100, 50, 250));
 
+    rectangle.setPosition(500.0, 400.0);
+
+    rectangle.setFillColor(sf::Color(100, 50, 250));
     sf::ConvexShape triangle;
     triangle.setPointCount(3);
     triangle.setPoint(0, sf::Vector2f(0.0, 0.0));
@@ -112,18 +109,15 @@ int main() {
     triangle.setOutlineColor(sf::Color::Red);
     triangle.setOutlineThickness(2);
     triangle.setPosition(600.0, 100.0);
-
     sf::Clock clock;
 
     int rectangle_velocity_x = 50;
     int rectangle_velocity_y = 200;
     int rectangle_angular_velocity = 10;
     //    int rec2_velocity_x = 100;
-
     //    int rec2_velocity_y = 100;
 
     bool flag_y = false;
-
     bool flag_x = false;
 
     sf::Vector2f size(120.0, 60.0);
@@ -134,12 +128,10 @@ int main() {
     // run the program as long as the window is open
     while (window.isOpen()) {
         sf::Time elapsed = clock.restart();
-
         float dt = elapsed.asSeconds();
-
         sf::FloatRect rectangle_bounds = rectangle.getGlobalBounds();
-
         //        sf::FloatRect rec2_bounds = rec2.getGlobalBounds();
+
         if(rectangle_bounds.top <= 0){
             rectangle_velocity_y = abs(rectangle_velocity_y);
             rectangle.setFillColor(sf::Color(rand() % 256, rand() % 256, rand() % 256));
@@ -158,11 +150,10 @@ int main() {
         }
         rectangle.move(rectangle_velocity_x*dt,rectangle_velocity_y*dt);
         rectangle.rotate(rectangle_angular_velocity*dt);
-
         rec2.animate(dt, window.getSize().y, window.getSize().x);
+
         rec2.moveInDirection();
         rec2.ObjectCollision(rectangle);
-
         // check all the window's events that were triggered since the last iteration of the loop
         sf::Event event;
         while (window.pollEvent(event)) {
@@ -172,7 +163,6 @@ int main() {
         }
         // clear the window with black color
         window.clear(sf::Color::Black);
-
         // draw everything here...
         window.draw(circle);
         window.draw(rectangle);
