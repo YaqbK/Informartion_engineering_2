6b507bee7f03d39c328f7cf57a7efdbbc6c465a3
Jakub Krusicki <kkrusicki@gmail.com>
2024-05-01 21:50:10 +0200

    copied from prev tasks
    copied and deleted unnecessary stuff

--- Labirynth/Labirynth/Labirynth.pro
+++ Labirynth/Labirynth/Labirynth.pro
@@ -3,5 +3,14 @@ CONFIG += console c++17
 CONFIG -= app_bundle
 CONFIG -= qt
 
+INCLUDEPATH += "C:/SFML/SFML-2.6.1-windows-gcc-13.1.0-mingw-64-bit/SFML-2.6.1/include"
+
+LIBS += -L"C:/SFML/SFML-2.6.1-windows-gcc-13.1.0-mingw-64-bit/SFML-2.6.1/lib"
+CONFIG(debug, debug|release){
+    LIBS += -lsfml-audio-d -lsfml-graphics-d -lsfml-network-d -lsfml-system-d -lsfml-window-d
+} else {
+    LIBS += -lsfml-audio -lsfml-graphics -lsfml-network -lsfml-system -lsfml-window
+}
+
 SOURCES += \
         main.cpp
--- Labirynth/Labirynth/main.cpp
+++ Labirynth/Labirynth/main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
 
-using namespace std;
+int main() {
+    // create the window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Labirynth");
 
-int main()
-{
-    cout << "Hello World!" << endl;
+    // run the program as long as the window is open
+    while (window.isOpen()) {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        // clear the window with black color
+        window.clear(sf::Color::Black);
+
+        // draw everything here...
+
+        // end the current frame
+        window.display();
+    }
+
     return 0;
 }
