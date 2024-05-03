aad841cbf03b1a52b31578c8f0cc55ad549ec83a
Jakub Krusicki <kkrusicki@gmail.com>
2024-05-03 18:31:41 +0200

    update main
    made the  vector of unique pointers to drawable

--- Labirynth/Labirynth/main.cpp
+++ Labirynth/Labirynth/main.cpp
@@ -2,22 +2,31 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 #include <memory>
+#include <vector>
 
 int main() {
     // create the window
     sf::RenderWindow window(sf::VideoMode(800, 600), "Labirynth");
 
+    std::vector<std::unique_ptr<sf::Drawable>> shapes;
+
     sf::Texture grass_texture;
     if (!grass_texture.loadFromFile("D:/Documents/Projects/PUT/Informartion_engineering_2/Labirynth/Labirynth/images/grass.png")) {return 1;}
     grass_texture.setRepeated(true);
-    sf::Sprite grass;
-    grass.setTexture(grass_texture);
-    grass.setTextureRect(sf::IntRect(0, 0, window.getSize().x, window.getSize().y));
+    std::unique_ptr<sf::Sprite> grass = std::make_unique<sf::Sprite>();
+    grass->setTexture(grass_texture);
+    grass->setTextureRect(sf::IntRect(0, 0, window.getSize().x, window.getSize().y));
+    shapes.emplace_back(std::move(grass));
 
     sf::Texture wall_texture;
     if (!wall_texture.loadFromFile("D:/Documents/Projects/PUT/Informartion_engineering_2/Labirynth/Labirynth/images/wall.png")) {return 1;}
-    sf::Sprite wall;
-    wall.setTexture(wall_texture);
+    wall_texture.setRepeated(true);
+    int numOfWalls = 10;
+    std::vector<sf::Sprite> walls(numOfWalls);
+    for(int i=0; i<numOfWalls; i++){
+        walls[i].setTexture(wall_texture);
+    }
+    walls[0].setTextureRect(sf::IntRect(0, 0, window.getSize().x, 30));
 
     sf::Texture guy_texture;
     if (!guy_texture.loadFromFile("D:/Documents/Projects/PUT/Informartion_engineering_2/Labirynth/Labirynth/images/guy.png")) {return 1;}
@@ -34,15 +43,16 @@ int main() {
                 window.close();
         }
 
-
-
         // clear the window with black color
         window.clear(sf::Color::Black);
 
         // draw everything here...
 //        grass.setTexture(grass_texture);
 //        grass.setTextureRect(sf::IntRect(100, 200, 200, 150));
-        window.draw(grass);
+        for(auto &s : shapes) {
+            window.draw(*s);
+        }
+        window.draw(walls[0]);
 
         // end the current frame
         window.display();
